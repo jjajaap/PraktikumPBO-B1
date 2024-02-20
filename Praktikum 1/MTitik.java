7d36c9e3289db32d60eb7badf66d1792be3a0ee4
jjajaap <117694668+jjajaap@users.noreply.github.com>
2024-02-20 11:37:11 +0700

    Update MTitik.java

--- Praktikum 1/MTitik.java
+++ Praktikum 1/MTitik.java
@@ -10,15 +10,13 @@ class MTitik {
     T2.setAbsis(3.0);
     T2.setOrdinat(4.0);
 
-    Titik T3 = new Titik();
-    T3.setAbsis(5.0);
-    T3.setOrdinat(6.0);
+    Titik T3 = new Titik(5,6);
+//    T3.setAbsis(5.0);
+//    T3.setOrdinat(6.0);
 
     System.out.println("Jumlah objek titik: " + T1.getCounterTitik());
     System.out.println("T1(" + T1.getAbsis() + "," + T1.getOrdinat() + ")");
-    System.out.println("Jumlah objek titik: " + T2.getCounterTitik());
     System.out.println("T1(" + T2.getAbsis() + "," + T2.getOrdinat() + ")");
-    System.out.println("Jumlah objek titik: " + T3.getCounterTitik());
     System.out.println("T1(" + T3.getAbsis() + "," + T3.getOrdinat() + ")");
     }
 }
