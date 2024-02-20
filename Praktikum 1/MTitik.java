6519a40bdc8e394acdd09480a5a058acd5505391
jjajaap <117694668+jjajaap@users.noreply.github.com>
2024-02-20 11:26:06 +0700

    Add files via upload

---
+++ Praktikum 1/MTitik.java
@@ -0,0 +1,24 @@
+class MTitik {
+    public static void main (String[] args){
+    
+    // deklarasi objek T1 dan T2 dari kelas Titik
+    Titik T1  = new Titik();
+    T1.setAbsis(1.0);
+    T1.setOrdinat(2.0);
+
+    Titik T2 = new Titik();
+    T2.setAbsis(3.0);
+    T2.setOrdinat(4.0);
+
+    Titik T3 = new Titik();
+    T3.setAbsis(5.0);
+    T3.setOrdinat(6.0);
+
+    System.out.println("Jumlah objek titik: " + T1.getCounterTitik());
+    System.out.println("T1(" + T1.getAbsis() + "," + T1.getOrdinat() + ")");
+    System.out.println("Jumlah objek titik: " + T2.getCounterTitik());
+    System.out.println("T1(" + T2.getAbsis() + "," + T2.getOrdinat() + ")");
+    System.out.println("Jumlah objek titik: " + T3.getCounterTitik());
+    System.out.println("T1(" + T3.getAbsis() + "," + T3.getOrdinat() + ")");
+    }
+}
---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,36 @@
+class Titik{
+    // deklarasi atribut 
+    double absis;
+    double ordinat;
+    static int counterTitik;
+
+    // deklarasi kontstruktor
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counterTitik++;
+    }
+    
+    // deklarasi method
+    void setAbsis(double a){
+        absis = a;
+    }
+    
+    void setOrdinat(double o){
+        ordinat= o;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    int getCounterTitik(){
+        return counterTitik;
+    }
+
+    
+}
